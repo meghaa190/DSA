44b50210ba07648e0df06fe450c4c179a85f1ade
MeghaChakraborty <92912235+meghaa190@users.noreply.github.com>
2022-10-31 11:11:59 +0530

    Update Stack using Array.cpp

--- Stack using Array.cpp
+++ Stack using Array.cpp
@@ -17,6 +17,15 @@ void pop() {
       top--;
    }
 }
+void display() {
+   if(top>=0) {
+      cout<<"Stack elements are:";
+      for(int i=top; i>=0; i--)
+      cout<<stack[i]<<" ";
+      cout<<endl;
+   } else
+   cout<<"Stack is empty";
+}
 
 int main() {
    int ch, val;
